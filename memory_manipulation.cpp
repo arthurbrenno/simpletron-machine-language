66ade085e0911d860de3c706d53588b7d17bdc04
Arthur-Brenno <64020210+arthurbrenno@users.noreply.github.com>
2023-05-09 17:40:47 -0300

    save #1

--- memory_manipulation.cpp
+++
@@ -1,9 +0,0 @@
-#include "memory_handler.h"
-
-bool Write(int& value, int& position, int mem[], int msize) {
-	if (position > msize || position < 0) {
-		return true;
-	}
-	mem[position] = value;
-	return true;
-}
